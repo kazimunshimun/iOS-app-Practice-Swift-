f5796f309aba94dbe83efd8c506b319be3459e0c
kazimunshimun <anik1.bd@gmail.com>
2019-03-15 23:33:39 +0600

    Welcome view controller added UI of welcomeVC done Connection between welcomeVC and loginVC, signUpVC done with button added

---
+++ Backpack/Project/Backpack/Backpack/Controller/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Backpack
+//
+//  Created by Anik on 15/3/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Backpack/Project/Backpack/Backpack/Controller/WelcomeViewController.swift
@@ -0,0 +1,85 @@
+//
+//  WelcomeViewController.swift
+//  Backpack
+//
+//  Created by Anik on 15/3/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var circleImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var isShowingBackground = false
+    //var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //timer.invalidate()
+        
+        //changeViewTimer.fire()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+        //timer = Timer(timeInterval: 5, target: self, selector: #selector(changeView), userInfo: nil, repeats: true)
+        
+        Timer.scheduledTimer(timeInterval: 10,
+                             target: self,
+                             selector: #selector(self.changeView),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    } 
+    
+    @objc func changeView() {
+        if isShowingBackground {
+            isShowingBackground = false
+            backgroundImageView.image = UIImage(named: "Splash_2")
+            circleImageView.image = UIImage(named: "eclipse_yellow")
+            descriptionLabel.textColor = UIColor.white
+        } else {
+            isShowingBackground = true
+            backgroundImageView.image = UIImage.from(color: UIColor.white)
+            circleImageView.image = UIImage(named: "eclipse")
+            descriptionLabel.textColor = UIColor.lightGray
+        }
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIImage {
+    static func from(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
