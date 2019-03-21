c467c5d9461ee69de9eec0005eb5f837f7b04c6c
kazimunshimun <anik1.bd@gmail.com>
2019-03-21 23:34:51 +0600

    Controllers of feed, messaging, trips. profile added Top view of these 4 view added

---
+++ Backpack/Project/Backpack/Backpack/Controller/MenuViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MenuViewController.swift
+//  Backpack
+//
+//  Created by Anik on 21/3/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+        //timer = Timer(timeInterval: 5, target: self, selector: #selector(changeView), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+        
+    } 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
