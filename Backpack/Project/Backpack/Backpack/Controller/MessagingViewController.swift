3d83dac060b474754d2a9695e73309c9bd9f0e9e
kazimunshimun <anik1.bd@gmail.com>
2019-03-29 20:31:03 +0600

    Chat model added Chat table view added

--- Backpack/Project/Backpack/Backpack/Controller/MessagingViewController.swift
+++ Backpack/Project/Backpack/Backpack/Controller/MessagingViewController.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 
-class MessagingViewController: UIViewController {
+class MessagingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var chatsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        chatsTableView.delegate = self
+        chatsTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell;
+    }
     /*
     // MARK: - Navigation
 
---
+++ Backpack/Project/Backpack/Backpack/Model/Chat.swift
@@ -0,0 +1,18 @@
+//
+//  Chat.swift
+//  Backpack
+//
+//  Created by Anik on 29/3/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import Foundation
+
+class Chats {
+    var id : String = ""
+    let name : String = ""
+    var lastMessage : String = ""
+    var lastMessageTime : String = ""
+    var isActive : Bool = false
+    
+}
