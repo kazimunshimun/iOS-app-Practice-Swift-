ffebd1356d2e81ef1fd27ed4d06a9e0f193457ae
kazimunshimun <anik1.bd@gmail.com>
2019-03-29 14:52:46 +0600

    Add Trips photo vc added

---
+++ Backpack/Project/Backpack/Backpack/Controller/Trips/AddTripsPhotoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddTripsPhotoViewController.swift
+//  Backpack
+//
+//  Created by Anik on 29/3/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class AddTripsPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        navigationController!.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
