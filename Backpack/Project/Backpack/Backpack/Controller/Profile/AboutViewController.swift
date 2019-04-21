360c094dabe5042c96c4acb9641877bfd0c0dadc
kazimunshimun <anik1.bd@gmail.com>
2019-04-22 00:05:36 +0600

    far table view added need modification

--- Backpack/Project/Backpack/Backpack/Controller/Profile/AboutViewController.swift
+++ Backpack/Project/Backpack/Backpack/Controller/Profile/AboutViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var faqTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        faqTableView.delegate = self
+        faqTableView.dataSource = self
+        faqTableView.register(UINib(nibName: "FAQTableViewCell", bundle: nil), forCellReuseIdentifier: "faqCell")
+        faqTableView.rowHeight = UITableView.automaticDimension
+        faqTableView.estimatedRowHeight = UITableView.automaticDimension
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
         navigationController!.popViewController(animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //var cell : UITableViewCell;
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "faqCell") as! FAQTableViewCell
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Backpack/Project/Backpack/Backpack/View/TableViewCells/FAQTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FAQTableViewCell.swift
+//  Backpack
+//
+//  Created by Anik on 21/4/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
