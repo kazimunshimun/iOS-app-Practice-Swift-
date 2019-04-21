94b335baf2d691c37b13f9f41e7ca4dc1109d7e4
kazimunshimun <anik1.bd@gmail.com>
2019-04-21 23:03:32 +0600

    privacy and terms view added far view added need modification

---
+++ Backpack/Project/Backpack/Backpack/Controller/Profile/PrivacyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PrivacyViewController.swift
+//  Backpack
+//
+//  Created by Anik on 21/4/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        navigationController!.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Backpack/Project/Backpack/Backpack/Controller/Profile/SettingsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SettingsViewController.swift
+//  Backpack
+//
+//  Created by Anik on 21/4/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var deactiveAccountView: UIView!
+    @IBOutlet weak var termsView: UIView!
+    @IBOutlet weak var privacyView: UIView!
+    @IBOutlet weak var aboutView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //deactiveAccountView.addTapGesture(tapNumber: 1, target: self, action: #selector(followersViewTouched))
+        termsView.addTapGesture(tapNumber: 1, target: self, action: #selector(termViewTouched))
+        privacyView.addTapGesture(tapNumber: 1, target: self, action: #selector(privacyViewTouched))
+        aboutView.addTapGesture(tapNumber: 1, target: self, action: #selector(aboutViewTouched))
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        navigationController!.popViewController(animated: true)
+    }
+    
+    @objc func termViewTouched() -> Void {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+        let termsViewController = storyBoard.instantiateViewController(withIdentifier: "termsView")
+        self.show(termsViewController, sender: nil)
+    }
+    
+    @objc func privacyViewTouched() -> Void {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+        let privacyViewController = storyBoard.instantiateViewController(withIdentifier: "privacyView")
+        self.show(privacyViewController, sender: nil)
+    }
+    
+    @objc func aboutViewTouched() -> Void {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+        let aboutViewController = storyBoard.instantiateViewController(withIdentifier: "aboutView")
+        self.show(aboutViewController, sender: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
