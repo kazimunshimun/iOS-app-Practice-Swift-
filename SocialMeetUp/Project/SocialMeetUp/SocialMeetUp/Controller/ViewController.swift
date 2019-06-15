b9f514d24fd632d62855d30d47f98fd7f5616780
kazimunshimun <anik1.bd@gmail.com>
2019-06-15 22:13:10 +0600

    Social meetup add initial commit

---
+++ SocialMeetUp/Project/SocialMeetUp/SocialMeetUp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialMeetUp
+//
+//  Created by Anik on 15/6/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
