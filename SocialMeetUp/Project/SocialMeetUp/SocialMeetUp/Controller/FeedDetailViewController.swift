316093b748c005f4fbb82a18b75e5d341b6ef255
kazimunshimun <anik1.bd@gmail.com>
2019-06-27 00:01:04 +0600

    feed detail view updated

--- SocialMeetUp/Project/SocialMeetUp/SocialMeetUp/Controller/FeedDetailViewController.swift
+++ SocialMeetUp/Project/SocialMeetUp/SocialMeetUp/Controller/FeedDetailViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class FeedDetailViewController: UIViewController {
 
-    var feed: Feed?
+    var feed: Feed!
     
     @IBOutlet weak var topView: RoundedCornerView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var firstJoinedImageView: RoundedCornerImageView!
+    @IBOutlet weak var secondJoinedImageView: RoundedCornerImageView!
+    
+    @IBOutlet weak var firstRoundedView: RoundedCornerView!
+    @IBOutlet weak var secondRoundedView: RoundedCornerView!
+    
+    @IBOutlet weak var joinedCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +32,35 @@ class FeedDetailViewController: UIViewController {
     
     func updateUI() {
         topView.backgroundColor = feed?.feedColor
-        titleLabel.text = feed?.title
+        titleLabel.text = feed.title
+        
+        updateJoinedByView()
+    }
+    
+    func updateJoinedByView() {
+        switch feed?.joinedPeople.count {
+        case 0:
+            joinedCountLabel.text = "Be the first to join"
+            firstRoundedView.isHidden = true
+            secondRoundedView.isHidden = true
+        case 1:
+            joinedCountLabel.text = "\(feed.joinedPeople[0].name) joined"
+            firstRoundedView.isHidden = false
+            secondRoundedView.isHidden = true
+            firstJoinedImageView.image = UIImage(named: feed.joinedPeople[0].imageName)
+        case 2:
+            joinedCountLabel.text = "\(feed.joinedPeople[0].name) and \(feed.joinedPeople[1].name) joined"
+            firstRoundedView.isHidden = false
+            secondRoundedView.isHidden = false
+            firstJoinedImageView.image = UIImage(named: feed.joinedPeople[0].imageName)
+            secondJoinedImageView.image = UIImage(named: feed.joinedPeople[1].imageName)
+        default:
+            joinedCountLabel.text = "\(feed.joinedPeople[0].name), \(feed.joinedPeople[1].name) and \(feed.joinedPeople.count - 2) others"
+            firstRoundedView.isHidden = false
+            secondRoundedView.isHidden = false
+            firstJoinedImageView.image = UIImage(named: feed.joinedPeople[0].imageName)
+            secondJoinedImageView.image = UIImage(named: feed.joinedPeople[1].imageName)
+        }
     }
     
     @IBAction func backButtonClicked(_ sender: Any) {
