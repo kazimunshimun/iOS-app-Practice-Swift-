df7ef1d55c29277d40b72adf30944d27012b9310
kazimunshimun <anik1.bd@gmail.com>
2019-06-28 17:50:52 +0600

    add new location view updated

--- SocialMeetUp/Project/SocialMeetUp/SocialMeetUp/Controller/AddNewViewController.swift
+++ SocialMeetUp/Project/SocialMeetUp/SocialMeetUp/Controller/AddNewViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
+import GoogleMaps
 
 class AddNewViewController: UIViewController {
 
+    @IBOutlet weak var groupNameTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var descriptionTextField: SkyFloatingLabelTextField!
+    
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var locationRoundedCornerView: RoundedCornerView!
+    @IBOutlet weak var interestView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        updateLocationView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        locationRoundedCornerView.topRight = true
+        locationRoundedCornerView.bottomLeft = true
+    }
+    
+    func updateLocationView() {
+        let camera = GMSCameraPosition.camera(withLatitude: 45.5303, longitude: -73.749, zoom: 14.0)
+        mapView.camera = camera
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 45.5303, longitude: -73.749)
+        marker.icon = UIImage(named: "map_pin")
+        marker.map = mapView
+        
     }
     
     @IBAction func backButtonClicked(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func continueButtonClicked(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
