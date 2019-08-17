004da5c30abd74bb99df774b62c47cc81944070c
kazimunshimun <anik1.bd@gmail.com>
2019-08-17 22:13:41 +0600

    walkthrough views updated

--- Plexus/Project/Plexus/Plexus/Module/WalkThrough/WalkThroughViewController.swift
+++ Plexus/Project/Plexus/Plexus/Module/WalkThrough/WalkThroughViewController.swift
@@ -9,13 +9,54 @@
 //
 
 import UIKit
+import TweenController
 
-class WalkThroughViewController: UIViewController, WalkThroughViewProtocol {
+class WalkThroughViewController: UIViewController, WalkThroughViewProtocol, TutorialViewController {
 
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var buttonsContainerView: UIView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    private var hasAppeared = false
+    private var tweenController: TweenController!
+    private var scrollView: UIScrollView!
+    
 	var presenter: WalkThroughPresenterProtocol?
 
 	override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if !hasAppeared {
+            hasAppeared = true
+            
+            let (tc, scrollView) = TutorialBuilder.buildWithContainerViewController(self)
+            self.tweenController = tc
+            self.scrollView = scrollView
+            self.scrollView.delegate = self
+        }
+    }
 
+    private func updatePageControl() {
+        pageControl.currentPage = Int(round(scrollView.contentOffset.x / containerView.frame.width))
+    }
+}
+
+extension WalkThroughViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        tweenController.update(progress: Double(scrollView.twc_horizontalPageProgress))
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        updatePageControl()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            updatePageControl()
+        }
+    }
 }
--- Plexus/Project/Plexus/Pods/Target Support Files/Pods-Plexus/Pods-Plexus-frameworks.sh
+++ Plexus/Project/Plexus/Pods/Target Support Files/Pods-Plexus/Pods-Plexus-frameworks.sh
@@ -157,6 +157,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TweenController/TweenController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -164,6 +165,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TweenController/TweenController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
