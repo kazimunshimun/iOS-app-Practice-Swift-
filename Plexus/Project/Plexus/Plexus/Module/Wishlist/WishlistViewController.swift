2152d02b9f1edd2b39eb7bd2f9b8a1a104c17146
kazimunshimun <anik1.bd@gmail.com>
2019-08-19 20:40:04 +0600

    Wishlist view updated

--- Plexus/Project/Plexus/Plexus/Module/Wishlist/WishlistViewController.swift
+++ Plexus/Project/Plexus/Plexus/Module/Wishlist/WishlistViewController.swift
@@ -12,10 +12,35 @@ import UIKit
 
 class WishlistViewController: UIViewController, WishlistViewProtocol {
 
-	var presenter: WishlistPresenterProtocol?
+    @IBOutlet weak var wishlistTableView: UITableView!
+    var presenter: WishlistPresenterProtocol?
 
 	override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
     }
 
+    private func setupViews() {
+        wishlistTableView.delegate = self
+        wishlistTableView.dataSource = self
+        wishlistTableView.register(UINib(nibName: "WishlistCell", bundle: nil), forCellReuseIdentifier: "wishlistCell")
+    }
+}
+
+extension WishlistViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let cellWidth : CGFloat = 160.0;
+        return cellWidth;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "wishlistCell", for: indexPath) as! WishlistCell
+        
+        return cell
+    }
 }
---
+++ Plexus/Project/Plexus/Plexus/Views/TableViewCell/WishlistCell.swift
@@ -0,0 +1,24 @@
+//
+//  WishlistCell.swift
+//  Plexus
+//
+//  Created by Anik on 19/8/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class WishlistCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
