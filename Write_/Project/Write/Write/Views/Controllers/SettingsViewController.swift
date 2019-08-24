be82cc6e7470888eb88d4bed7f62f1bf5020b5f0
kazimunshimun <anik1.bd@gmail.com>
2019-08-24 22:30:47 +0600

    Settings view added

--- Write_/Project/Write/Write/Views/Controllers/SettingsViewController.swift
+++ Write_/Project/Write/Write/Views/Controllers/SettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +22,13 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            headerView.contentView.backgroundColor = .clear
+            headerView.backgroundView?.backgroundColor = .black
+            headerView.textLabel?.textColor = UIColor(white: 1.0, alpha: 0.5)
+            headerView.textLabel?.font = UIFont(name: "HelveticaNeue-Regular", size: 13)
+        }
     }
-    */
 
 }
