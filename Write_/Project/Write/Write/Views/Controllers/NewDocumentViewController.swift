73fa0aa9ca8955a46bb459f438d2730e3f605683
kazimunshimun <anik1.bd@gmail.com>
2019-08-27 19:27:34 +0600

    textview alignment added

--- Write_/Project/Write/Write/Views/Controllers/NewDocumentViewController.swift
+++ Write_/Project/Write/Write/Views/Controllers/NewDocumentViewController.swift
@@ -64,7 +64,77 @@ class NewDocumentViewController: UIViewController {
         }
         
     }
-
+    
+    
+    @IBAction func boldButtonClicked(_ sender: Any) {
+        if let range = documentTextView.selectedTextRange {
+            let selectedText = documentTextView.text(in: range)
+            if selectedText!.count > 0 {
+               // let paragraphStyle = NSMutableParagraphStyle()
+               // paragraphStyle.alignment = NSTextAlignment.center
+            } else {
+                let changedFontDescriptor = UIFont.systemFont(ofSize: (CGFloat)(15.0))
+                let typ = documentTextView.typingAttributes[NSAttributedString.Key.font] as? UIFont
+                if ( typ == changedFontDescriptor) {
+                    self.documentTextView.typingAttributes[NSAttributedString.Key.font] = UIFont.boldSystemFont(ofSize: (CGFloat)(15))
+                }else {
+                    self.documentTextView.typingAttributes[NSAttributedString.Key.font] = UIFont.systemFont(ofSize: (CGFloat)(15))
+                }
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func italicButtonClicked(_ sender: Any) {
+        let typ = documentTextView.typingAttributes[NSAttributedString.Key.obliqueness] as? NSNumber
+        if typ == 0.5 {
+            documentTextView.typingAttributes[NSAttributedString.Key.obliqueness] = 0
+        }else {
+            documentTextView.typingAttributes[NSAttributedString.Key.obliqueness] = 0.5
+        }
+    }
+    
+    @IBAction func underlineButtonClicked(_ sender: Any) {
+        let typ = documentTextView.typingAttributes[NSAttributedString.Key.underlineStyle] as? NSNumber
+        if (typ == 1) {
+            self.documentTextView.typingAttributes[NSAttributedString.Key.underlineStyle] = 0
+        } else {
+            self.documentTextView.typingAttributes[NSAttributedString.Key.underlineStyle] = 1
+        }
+    }
+    
+    @IBAction func textColorChangeButtonClicked(_ sender: Any) {
+        //let typ = documentTextView.typingAttributes[NSAttributedString.Key.] as? NSNumber
+    }
+    
+    @IBAction func leftAlignButtonClicked(_ sender: Any) {
+        setParagraphAlignment(newAlignmnet: .left)
+    }
+    
+    @IBAction func centerAlignButtonClicked(_ sender: Any) {
+        setParagraphAlignment(newAlignmnet: .center)
+    }
+    
+    @IBAction func rightAlignButtonClicked(_ sender: Any) {
+        setParagraphAlignment(newAlignmnet: .right)
+    }
+    
+    private func setParagraphAlignment(newAlignmnet: NSTextAlignment) {
+        if let range = documentTextView.selectedTextRange {
+            let selectedText = documentTextView.text(in: range)
+            if selectedText!.count > 0 {
+                let newParagraphStyle = NSMutableParagraphStyle.init()
+                newParagraphStyle.alignment = newAlignmnet
+                let dict = [NSAttributedString.Key.paragraphStyle: newParagraphStyle]
+                documentTextView.textStorage.beginEditing()
+                documentTextView.textStorage.setAttributes(dict, range: documentTextView.selectedRange)
+                documentTextView.textStorage.endEditing()
+            } else {
+                
+            }
+        }
+    }
 }
 
 extension NewDocumentViewController : UITextViewDelegate {
