47f6c967f89d8091567a7940633b25cc132a81ec
kazimunshimun <anik1.bd@gmail.com>
2019-08-28 23:42:56 +0600

    blink animation added

--- Write_/Project/Write/Write/Views/Controllers/ViewController.swift
+++ Write_/Project/Write/Write/Views/Controllers/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blinkView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startBlink()
     }
 
-
+    func startBlink() {
+        UIView.animate(withDuration: 0.7,//Time duration
+            delay:0.0,
+            options:[.allowUserInteraction, .curveEaseInOut, .autoreverse, .repeat],
+            animations: {
+                UIView.setAnimationRepeatCount(3)
+                self.blinkView.alpha = 0.0
+        }, completion: { (finished: Bool) in
+            self.startSignInAnimation()
+        })
+    }
+    
+    private func startSignInAnimation() {
+        print("sign in animation started")
+    }
 }
 
