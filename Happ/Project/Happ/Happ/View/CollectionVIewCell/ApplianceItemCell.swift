89f334c6aebf2ac00b9dda167b80306f6d7424f9
kazimunshimun <anik1.bd@gmail.com>
2019-09-10 19:51:01 +0600

    Appliance item collection view updated

---
+++ Happ/Project/Happ/Happ/View/CollectionVIewCell/ApplianceItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ApplianceItemCell.swift
+//  Happ
+//
+//  Created by Anik on 10/9/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class ApplianceItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemTitle: UILabel!
+}
--- Happ/Project/Happ/Happ/View/TableViewCell/ApplianceCell.swift
+++ Happ/Project/Happ/Happ/View/TableViewCell/ApplianceCell.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ApplianceCell: UITableViewCell {
 
+    @IBOutlet weak var applianceCollectionView: UICollectionView!
+    
+    let items = ["Air Conditioner", "Smart Light", "Refrigerator", "LED Bulb"]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        applianceCollectionView.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,4 +26,18 @@ class ApplianceCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension ApplianceCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! ApplianceItemCell
+        cell.itemTitle.text = items[indexPath.row]
+        return cell
+    }
+    
+    
 }
