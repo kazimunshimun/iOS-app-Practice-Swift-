efd5955133c5e89bcc3408c06eb136738ab1fbe2
kazimunshimun <anik1.bd@gmail.com>
2019-09-12 23:46:31 +0600

    light collection view updated

---
+++ Happ/Project/Happ/Happ/View/CollectionVIewCell/HueCell.swift
@@ -0,0 +1,14 @@
+//
+//  HueCell.swift
+//  Happ
+//
+//  Created by Anik on 12/9/19.
+//  Copyright © 2019 A7Studio. All rights reserved.
+//
+
+import UIKit
+
+class HueCell: UICollectionViewCell {
+    
+    @IBOutlet weak var hueColorView: UIView!
+}
--- Happ/Project/Happ/Happ/View/Controllers/LightControlViewController.swift
+++ Happ/Project/Happ/Happ/View/Controllers/LightControlViewController.swift
@@ -10,21 +10,47 @@ import UIKit
 
 class LightControlViewController: UIViewController {
 
+    @IBOutlet weak var brightnessSlider: TactileSlider!
+    @IBOutlet weak var brightnessLabel: UILabel!
+    @IBOutlet weak var valueView: UIView!
+    @IBOutlet weak var hueSelectionCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        hueSelectionCollectionView.delegate = self
+        hueSelectionCollectionView.dataSource = self
     }
     
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func brightnessChanged(_ sender: TactileSlider) {
+        setBrightnessValue(value: Int(sender.value))
+    }
+    
+    func setBrightnessValue(value: Int) {
+        brightnessLabel.text = "\(value)%"
+        valueView.frame.origin.y = brightnessSlider.frame.height - brightnessSlider.positionForValue(Float(value)) - 16
+    }
 
+}
+
+extension LightControlViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hueCell", for: indexPath) as! HueCell
+        //cell.itemTitle.text = items[indexPath.row]
+        return cell
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        applianceDelegate?.applianceSelected(name: items[indexPath.row])
     }
     */
-
 }
